d62c3829ccdc6df32f6c26915de77b4cf5a59637
Joan Galmés Riera <joan.galmes@gmail.com>
2023-09-25 22:07:48 +0200

    bloqueig mentre es sincronitza

--- src/service/CentreService.ts
+++ src/service/CentreService.ts
@@ -4,8 +4,12 @@ import {Calendari} from "src/model/google/Calendari";
 
 export class CentreService {
   static async isSincronitzant(): Promise<Boolean> {
-    const response = await axios.post(process.env.API + '/api/core/centre/sincronitzant');
-    const data:any = await response.data;
-    return data
+    try {
+      const response = await axios.post(process.env.API + '/api/core/public/centre/sincronitzant');
+      const data: any = await response.data;
+      return data
+    } catch (e:any){
+      return false;
+    }
   }
 }
